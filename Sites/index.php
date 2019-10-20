39a8b2675e9f66a0b1109675e4495dd9a63285f0
Carlos Quezada <ciquezada@uc.cl>
2019-10-20 12:43:02 -0300

    pequeña actualizacion

--- Sites/index.php
+++ Sites/index.php
@@ -7,9 +7,9 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow" onclick="window.location.href= 'views/proyectos/index.php'; ">
-              <img src="https://freedesignfile.com/upload/2015/12/Oil-refinery-industry-logo-vector-04.jpg" class="card-img-top" alt="...">
+          <div class="col-xl-6 col-md-6 mb-4">
+            <div class="card border-0 shadow" onclick="window.location.href= 'views/pages/proyectos_index.php'; ">
+              <img src="https://www.clipartwiki.com/clipimg/detail/26-262092_onlinelabels-clip-art-nuclear-nuclear-power-plant-logo.png" class="card-img-top" alt="...">
               <div class="card-body text-center">
                 <h5 class="card-title mb-0">Proyectos</h5>
                 <div class="card-text text-black-50">Conoce los proyectos</div>
@@ -17,32 +17,12 @@
             </div>
           </div>
 
-          <div class="col-xl-3 col-md-6 mb-4">
+          <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-0 shadow" onclick="window.location.href= 'views/recursos/index.php'; ">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlEj7SY4niuNHYTNQ-r2jLqlpf1qTVdtunaS3AE2TxD-Y3azBZ_Q" class="card-img-top" alt="...">
+              <img src="http://www.falco.lk/wp-content/uploads/2018/04/Falco-green.png" class="card-img-top" alt="...">
               <div class="card-body text-center">
-                <h5 class="card-title mb-0">Recursos</h5>
-                <div class="card-text text-black-50">Conoce los recursos</div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow" onclick="window.location.href= 'views/socios/socios.php'; ">
-              <img src="https://blackphoenixalchemylab.com/wp-content/uploads/2012/12/Jolly-Roger.jpg" class="card-img-top" alt="...">
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">Socios</h5>
-                <div class="card-text text-black-50">Socios con recursos pendientes en sus proyectos</div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-0 shadow" onclick="window.location.href= 'views/lugares/lugares.php'; ">
-              <img src="https://wpforms.com/wp-content/uploads/2016/08/addon-icon-geolocation.png" class="card-img-top" alt="...">
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0">Lugares</h5>
-                <div class="card-text text-black-50">Conoce las regiones con proyectos</div>
+                <h5 class="card-title mb-0">ONGs</h5>
+                <div class="card-text text-black-50">Conoce las ONG</div>
               </div>
             </div>
           </div>
